c84586b8266d72cb402e9f5eaf374d0b72a588e1
robertwendel7 <134088034+robertwendel7@users.noreply.github.com>
2024-07-05 19:39:19 +0000

    segundo commit

--- campo/loja/models.py
+++ campo/loja/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 from django.utils import timezone
 
-class Produtos(models.Model):
+class Produto(models.Model):
 
-    name = models .CharField(max_length=200)
+    nome = models.CharField(max_length=100)
+    preco = models.IntegerField()
 
+    def __str__(self):
+        return self.nome
+
+class Fornecedore(models.Model):
+    nome = models.CharField(max_length=200)   
+
+    def __str__(self):
+        return self.nome       
 # Create your models here.
--- campo/loja/views.py
+++ campo/loja/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Produto
 
 def pesquisar(request):
 
+    resultado = Produto.objects.filter(nome = 'manga')
+    print(resultado)
     print("Pesquisando...")
-    return HttpResponse("Pesquisando...")
+    return HttpResponse(resultado)
+
+
+
+
+
+
+
+
+
 # Create your views here.
